fcd6aa4e7b7bfe1340686cea502e64c79ce22c48
Fiore Basile <fiore.basile@gmail.com>
2017-06-21 18:44:31 +0200

    Fixed bug in loading when not logged in

--- src/apps/instructor/Boot.vue
+++ src/apps/instructor/Boot.vue
@@ -1,99 +1,96 @@
 <template>
     <div v-if="loading">
-         <el-row>
-        <el-col :span="12" :offset="6">
-           <el-card>
-            <h1>Loading...</h1>
-            <p>Please wait, loading your dashboard</p>
-            <el-progress :percentage="loaded"></el-progress>
-            </el-card>
-        </el-col>
-      </el-row>
+        <el-row>
+            <el-col :span="12" :offset="6">
+                <el-card>
+                    <h1>Loading...</h1>
+                    <p>Please wait, loading your dashboard</p>
+                    <el-progress :percentage="loaded"></el-progress>
+                </el-card>
+            </el-col>
+        </el-row>
     </div>
     <div v-else>
         <div v-if="selectedRole">
-                <router-view></router-view>
-            </div>
-
-            <div v-else>
-                 <el-row v-if="filteredRoles.length > 1">
-                <el-col :span="12" :offset="6">
-                <el-card>
-                    <p>Looks like you are instructor in more than one course</p>
-                    <p>Please select a course</p>
-                <el-select v-model="selectedRole">
-                    <el-option v-for="role in filteredRoles"  :label="role.instructor.course.name" :value="role" :key="role.role"></el-option>
-                </el-select>
-                </el-card>
-                 </el-col>
-                </el-row>
-            </div>
+            <router-view></router-view>
         </div>
-     </div>
-
-                
-
+    
+        <div v-else>
+            <el-row v-if="filteredRoles.length > 1">
+                <el-col :span="12" :offset="6">
+                    <el-card>
+                        <p>Looks like you are instructor in more than one course</p>
+                        <p>Please select a course</p>
+                        <el-select v-model="selectedRole">
+                            <el-option v-for="role in filteredRoles" :label="role.instructor.course.name" :value="role" :key="role.role"></el-option>
+                        </el-select>
+                    </el-card>
+                </el-col>
+            </el-row>
+        </div>
+    </div>
+    </div>
 </template>
 <script>
-import {mapGetters,mapActions} from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
-  data() {
-      return {
-          loading: false,
-          loaded: 0,
-          selectedRole: null,
-          showSelect: false
-      }
-  },
-  computed: {
-      ...mapGetters({
-          userRoles: 'apps/userRoles'
-      }),
-      filteredRoles(){
-          const roles = this.userRoles || [];
-          return roles.filter((el)=>{
-              return (el.role === 'instructor')
-          })
-      }
-  },
-  methods: {
-      launchApp: function(){
-        //   alert('boot')
-        var vm = this;
-        this.bootApp(this.selectedRole.instructor).then((success) => {
-            vm.loading=false;
-            vm.loaded=100;
-            // vm.$nextTick(function(){
-                vm.$router.push('/apps/instructor/d/home')
-            // });
-        }).catch((error)=>{
-            vm.loading=false;
-            vm.loaded=0;
-            // vm.$nextTick(function(){
-                vm.$router.push('/apps/instructor/error')
-            // });
-        })
-     },
-     ...mapActions({
-          bootApp : 'instructor_app/bootApp',
-          loadInstalledApps: 'apps/loadInstalledApps'
-      })
-  },
-  mounted(){
-      const vm = this;
-      this.loadInstalledApps().then((success)=>{
-        if (vm.filteredRoles.length == 1) {
-            vm.selectedRole = vm.filteredRoles[0];
-            vm.$nextTick(function(){
-                vm.launchApp()
+    data() {
+        return {
+            loading: false,
+            loaded: 0,
+            selectedRole: null,
+            showSelect: false
+        }
+    },
+    computed: {
+        ...mapGetters({
+            userRoles: 'apps/userRoles'
+        }),
+        filteredRoles() {
+            const roles = this.userRoles || [];
+            return roles.filter((el) => {
+                return (el.role === 'instructor')
             })
         }
-      }).catch((error)=>{
-         console.log('Something bad happened');
-         vm.router.push('/') 
-      });
-  }
+    },
+    methods: {
+        launchApp: function () {
+            //   alert('boot')
+            var vm = this;
+            this.bootApp(this.selectedRole.instructor).then((success) => {
+                vm.loading = false;
+                vm.loaded = 100;
+                // vm.$nextTick(function(){
+                vm.$router.push('/apps/instructor/d/home')
+                // });
+            }).catch((error) => {
+                vm.loading = false;
+                vm.loaded = 0;
+                // vm.$nextTick(function(){
+                vm.$router.push('/apps/instructor/error')
+                // });
+            })
+        },
+        ...mapActions({
+            bootApp: 'instructor_app/bootApp',
+            loadInstalledApps: 'apps/loadInstalledApps'
+        })
+    },
+    mounted() {
+        const vm = this;
+        this.loadInstalledApps().then((success) => {
+            if (vm.filteredRoles.length == 1) {
+                vm.selectedRole = vm.filteredRoles[0];
+                vm.$nextTick(function () {
+                    vm.launchApp()
+                })
+            }
+        }).catch((error) => {
+            console.log('Something bad happened');
+            vm.$router.push('/')
+        });
+    }
 }
 </script>
 

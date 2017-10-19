4576addc0b57302a100e26badf16f4549ad636a4
Fiore Basile <fiore.basile@gmail.com>
2017-10-19 11:54:37 +0200

    fixed bug in labs manager

--- src/apps/manager/labs/Detail.vue
+++ src/apps/manager/labs/Detail.vue
@@ -1,121 +1,123 @@
 <template>
-
-<div class="form clearfix">
-  <h3><span v-if="form.id">Edit</span>
-    <span v-if="!form.id">Fill the form to create a new</span>
-     Lab <span v-if="form.id">{{form.lab_id}}</span> </h3>
-  <el-form ref="myForm" :rules="rules" :model="form" label-position="left" label-width="150px" >
-    <el-form-item label="Lab name" prop="name">
-      <el-input v-model="form.name"></el-input>
-    </el-form-item>
-    <el-form-item label="Country" prop="country">
-      <el-input v-model="form.country"></el-input>
-    </el-form-item>
-    <el-form-item label="Continent" prop="continent">
-      <el-input v-model="form.continent"></el-input>
-    </el-form-item>
-    <el-form-item label="Path" prop="lab_id">
-       <el-input v-model="form.lab_id"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button v-if="!form.id" type="primary" @click="submitForm('myForm')">Create</el-button>
-      <el-button v-if="form.id" type="primary" @click="submitForm('myForm')">Save</el-button>
-      <el-button @click="resetForm('myForm')">Cancel</el-button>
-    </el-form-item>
-</el-form>
-</div>
+  <div class="form clearfix">
+    <h3>
+      <span v-if="form.id">Edit</span>
+      <span v-if="!form.id">Fill the form to create a new</span>
+      Lab
+      <span v-if="form.id">{{form.lab_id}}</span>
+    </h3>
+    <el-form ref="myForm" :rules="rules" :model="form" label-position="left" label-width="150px">
+      <el-form-item label="Lab name" prop="name">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="Country" prop="country">
+        <el-input v-model="form.country"></el-input>
+      </el-form-item>
+      <el-form-item label="Continent" prop="continent">
+        <el-input v-model="form.continent"></el-input>
+      </el-form-item>
+      <el-form-item label="Path" prop="lab_id">
+        <el-input v-model="form.lab_id"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button v-if="!form.id" type="primary" @click="submitForm('myForm','create')">Create</el-button>
+        <el-button v-if="form.id" type="primary" @click="submitForm('myForm')">Save</el-button>
+        <el-button @click="resetForm('myForm')">Cancel</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 
 <script>
-import {mapActions,mapGetters} from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 export default {
-    props: [
-      'item'
-    ],
-    data() {
-      return {
-        rules: {
-              name: [
-                   { required: true, message: 'Please enter Lab Name', trigger: 'blur' }
-              ],
-              lab_id: [
-                   { required: true, message: 'Please enter Lab ID', trigger: 'blur' }
-              ],
-              country: [
-                   { required: true, message: 'Please select Country', trigger: 'blur' }
-              ],
-              continent: [
-                   { required: true, message: 'Please select Continent', trigger: 'blur' }
-              ]
-        },
+  props: [
+    'item'
+  ],
+  data() {
+    return {
+      rules: {
+        name: [
+          { required: true, message: 'Please enter Lab Name', trigger: 'blur' }
+        ],
+        lab_id: [
+          { required: true, message: 'Please enter Lab ID', trigger: 'blur' }
+        ],
+        country: [
+          { required: true, message: 'Please select Country', trigger: 'blur' }
+        ],
+        continent: [
+          { required: true, message: 'Please select Continent', trigger: 'blur' }
+        ]
+      },
 
     }
   },
   computed: {
-      form: function(){ return  this.item || {} },
-      ...mapGetters(['currentCourse'])
+    form: function() { return this.item || {} },
+    ...mapGetters(['currentCourse'])
   },
-    methods: {
-      ...mapActions(['createLab','updateLab']),
-      submitForm(formName,mode) {
-        var vm = this;
-        var formW =this.$refs[formName];
-        formW.validate((valid) => {
-          if (valid) {
-            if (mode == 'create') {
-              this.createLab({
-                courseId: this.currentCourse.id,
-                course: this.currentCourse,
-                name: this.item.name,
-                lab_id: this.item.lab_id,
-                country: this.item.country,
-                continent: this.item.continent
-              }).then(function(success){
-                vm.notify('Success', 'New lab created');
-                vm.resetForm();
-                //formW.resetFields();
-              }).catch(function(error){
-                vm.notify('Error', 'Some error happened');
-              });
-            } else {
-              this.updateLab({
-                id: this.item.id,
-                courseId: this.currentCourse.id,
-                course: this.currentCourse,
-                name: this.item.name,
-                lab_id: this.item.lab_id,
-                country: this.item.country,
-                continent: this.item.continent
-              }).then(function(success){
-                vm.notify('Success', 'Lab updated');
-                vm.resetForm();
-                //formW.resetFields();
-              }).catch(function(error){
-                console.log(error);
-                vm.notify('Error', 'Some error happened');
-              });
-            }
+  methods: {
+    ...mapActions(['createLab', 'updateLab']),
+    submitForm(formName, mode) {
+      var vm = this;
+      var formW = this.$refs[formName];
+      formW.validate((valid) => {
+        if (valid) {
+          if (mode === 'create') {
+            this.createLab({
+              courseId: this.currentCourse.id,
+              // course: this.currentCourse,
+              name: this.item.name,
+              lab_id: this.item.lab_id,
+              country: this.item.country,
+              continent: this.item.continent
+            }).then(function(success) {
+              vm.notify('Success', 'New lab created');
+              vm.resetForm();
+              //formW.resetFields();
+            }).catch(function(error) {
+              vm.notify('Error', 'Some error happened');
+            });
           } else {
-            vm.notify('Error', 'Form is not valid');
-            return false;
+            this.updateLab({
+              id: this.item.id,
+              courseId: this.currentCourse.id,
+              // course: this.currentCourse,
+              name: this.item.name,
+              lab_id: this.item.lab_id,
+              country: this.item.country,
+              continent: this.item.continent
+            }).then(function(success) {
+              vm.notify('Success', 'Lab updated');
+              vm.resetForm();
+              //formW.resetFields();
+            }).catch(function(error) {
+              console.log(error);
+              vm.notify('Error', 'Some error happened');
+            });
           }
-        });
-      },
-      notify(title, msg){
-        const h = this.$createElement;
+        } else {
+          vm.notify('Error', 'Form is not valid');
+          return false;
+        }
+      });
+    },
+    notify(title, msg) {
+      const h = this.$createElement;
 
-        this.$notify({
-          title: title,
-          message: h('i', { style: 'color: teal' }, msg)
-        });
-      },
-      resetForm(formName) {
-        //this.$refs[formName].resetFields();
-        this.$emit('cancelEdit');
-      },
-    }
+      this.$notify({
+        title: title,
+        message: h('i', { style: 'color: teal' }, msg)
+      });
+    },
+    resetForm(formName) {
+      //this.$refs[formName].resetFields();
+      this.$emit('cancelEdit');
+    },
   }
+}
 </script>
 <style scoped>
 .form {
--- src/store/modules/labs.js
+++ src/store/modules/labs.js
@@ -10,170 +10,173 @@ import api from './api'
 
 
 export default {
-  getters: {
-    labs: state => state.labs,
-  },
-  mutations: {
-    [LOAD_LABS](state) {
+    getters: {
+        labs: state => state.labs,
+    },
+    mutations: {
+        [LOAD_LABS](state) {
             // nop
-    },
-    [SET_LABS](state, labs) {
-      state.labs = labs;
-    },
-    [CREATE_LAB](state, response) {
-      var newc = state.labs.slice();
-      newc.push(response);
-      state.labs = newc;
-    },
-    [UPDATE_LAB](state, response) {
-      let newc = [];
-      state.labs.forEach((el) => {
-        if (el.id === response.id) {
-          newc.push(response);
-        } else {
-          newc.push(el)
-        }
-      });
-      state.labs = newc;
-    },
-    [DELETE_LAB](state, response) {
-      let newc = [];
-      state.labs.forEach((el) => {
-        if (el.id === response.id) {
+        },
+        [SET_LABS](state, labs) {
+            state.labs = labs;
+        },
+        [CREATE_LAB](state, response) {
+            var newc = state.labs.slice();
+            newc.push(response);
+            state.labs = newc;
+        },
+        [UPDATE_LAB](state, response) {
+            let newc = [];
+            state.labs.forEach((el) => {
+                if (el.id === response.id) {
+                    newc.push(response);
+                } else {
+                    newc.push(el)
+                }
+            });
+            state.labs = newc;
+        },
+        [DELETE_LAB](state, response) {
+            let newc = [];
+            state.labs.forEach((el) => {
+                if (el.id === response.id) {
                     // newc.push(response);
-        } else {
-          newc.push(el)
-        }
-      });
-      state.labs = newc;
+                } else {
+                    newc.push(el)
+                }
+            });
+            state.labs = newc;
+        },
+        [ASSIGN_LAB](state, response) {},
     },
-    [ASSIGN_LAB](state, response) {},
-  },
-  actions: {
-    assignLab({
-            commit, dispatch,
+    actions: {
+        assignLab({
+            commit,
+            dispatch,
         }, params) {
-      const { instructor, lab } = params
-      console.log('Assing ' + instructor.username + ' to ' + lab.lab_id)
-      const opts = {
-        url: '/api/instructors/' + instructor.id + '/labs/rel/' + lab.id,
-      }
-      api.put(opts.url, {
-        instructorId: instructor.id,
-        labId: lab.id,
-      })
-            .then((response) => {
-              console.log(response);
-              commit(ASSIGN_LAB, response);
-              dispatch('loadLabs', instructor.courseId)
-            }).catch((error) => {
-              console.log(error)
-              commit('API_FAILURE', error, { root: true })
-            })
-    },
-    loadLabs({
+            const { instructor, lab } = params
+            console.log('Assing ' + instructor.username + ' to ' + lab.lab_id)
+            const opts = {
+                url: '/api/instructors/' + instructor.id + '/labs/rel/' + lab.id,
+            }
+            api.put(opts.url, {
+                    instructorId: instructor.id,
+                    labId: lab.id,
+                })
+                .then((response) => {
+                    console.log(response);
+                    commit(ASSIGN_LAB, response);
+                    dispatch('loadLabs', instructor.courseId)
+                }).catch((error) => {
+                    console.log(error)
+                    commit('API_FAILURE', error, { root: true })
+                })
+        },
+        loadLabs({
             commit,
         }, courseId) {
-      commit(LOAD_LABS);
-      const opts = {
-        url: '/api/courses/' + courseId + '/labs?filter=' + encodeURIComponent(
+            commit(LOAD_LABS);
+            const opts = {
+                url: '/api/courses/' + courseId + '/labs?filter=' + encodeURIComponent(
                     JSON.stringify({
-                      include: [
-                        'course',
-                        'students',
-                        'instructors',
-                        'supernode',
-                      ],
+                        include: [
+                            'course',
+                            'students',
+                            'instructors',
+                            'supernode',
+                        ],
                     })
                 ),
-      }
-      api.get(opts.url)
-                .then(function (response) {
+            }
+            api.get(opts.url)
+                .then(function(response) {
                     // console.log('set labs');
                     // console.log(response);
-                  commit('SET_LABS', response);
+                    commit('SET_LABS', response);
                 })
-                .catch(function (error) {
-                  commit('API_FAILURE', error)
-                    //  if (cb) cb(error);
+                .catch(function(error) {
+                    commit('API_FAILURE', error)
+                        //  if (cb) cb(error);
                 });
-    },
-    setLabs({
+        },
+        setLabs({
             commit,
         }, labs) {
-      commit(SET_LABS, labs);
-    },
-    createLab({
+            commit(SET_LABS, labs);
+        },
+        createLab({
+            commit,
+            dispatch
+        }, lab) {
+            return new Promise((resolve, reject) => {
+                debugger
+                const courseId = lab.courseId || (lab.course && lab.course.id)
+                    // Do something here... lets say, a http call using vue-resource
+                api.post("/api/courses/" + courseId + "/labs", lab).then((response) => {
+                    // http success, call the mutator and change something in state
+                    commit(CREATE_LAB, response.body)
+                        // Let the calling function know that http is done. You may send some data back
+                        // push('/apps/manager/courses')
+                    dispatch('loadLabs', courseId)
+                    resolve(response);
+                }, (error) => {
+                    // http failed, let the calling function know that action did not work out
+                    commit(API_FAILURE, error);
+                    // push('/apps/manager/courses')
+                    reject(error);
+                })
+            });
+        },
+        updateLab({
+            commit,
+            dispatch,
+        }, lab) {
+            return new Promise((resolve, reject) => {
+                debugger
+                const courseId = lab.courseId || lab.course && lab.course.id
+                if (!courseId) return reject("Can't find courseId")
+                    // Do something here... lets say, a http call using vue-resource
+                api.put("/api/courses/" + courseId + "/labs/" + lab.id, lab).then((response) => {
+                    // http success, call the mutator and change something in state
+                    commit(UPDATE_LAB, response.body)
+                    dispatch('loadLabs', courseId)
+                        // Let the calling function know that http is done. You may send some data back
+                        //  push('/apps/manager/courses')
+                    resolve(response);
+                }, (error) => {
+                    // http failed, let the calling function know that action did not work out
+                    commit(API_FAILURE, error);
+                    //  push('/apps/manager/courses')
+                    reject(error);
+                })
+            });
+        },
+        deleteLab({
             commit,
         }, lab) {
-            // random password, there's no login anyway
-      return new Promise((resolve, reject) => {
-        const courseId = lab.courseId || lab.course && lab.course.id
-
+            return new Promise((resolve, reject) => {
                 // Do something here... lets say, a http call using vue-resource
-        api.post("/api/courses/" + courseId + "/labs", lab).then((response) => {
-                    // http success, call the mutator and change something in state
-          commit(CREATE_LAB, response.body)
-                    // Let the calling function know that http is done. You may send some data back
-                    // push('/apps/manager/courses')
-          dispatch('loadLabs', courseId)
-          resolve(response);
-        }, (error) => {
-                    // http failed, let the calling function know that action did not work out
-          commit(API_FAILURE, error);
-                    // push('/apps/manager/courses')
-          reject(error);
-        })
-      });
-    },
-    updateLab({
-            commit, dispatch,
-        }, lab) {
-      return new Promise((resolve, reject) => {
-        const courseId = lab.courseId || lab.course && lab.course.id
-        if (!courseId) return reject("Can't find courseId")
-                // Do something here... lets say, a http call using vue-resource
-        api.put("/api/courses/" + courseId + "/labs/" + lab.id, lab).then((response) => {
-                    // http success, call the mutator and change something in state
-          commit(UPDATE_LAB, response.body)
-          dispatch('loadLabs', courseId)
-                    // Let the calling function know that http is done. You may send some data back
-                    //  push('/apps/manager/courses')
-          resolve(response);
-        }, (error) => {
-                    // http failed, let the calling function know that action did not work out
-          commit(API_FAILURE, error);
-                    //  push('/apps/manager/courses')
-          reject(error);
-        })
-      });
-    },
-    deleteLab({
-            commit,
-        }, lab) {
-      return new Promise((resolve, reject) => {
-                // Do something here... lets say, a http call using vue-resource
-        const courseId = lab.courseId || lab.course.id
-        if (!courseId) return reject("Can't find courseId")
+                const courseId = lab.courseId || lab.course.id
+                if (!courseId) return reject("Can't find courseId")
 
-        api.delete("/api/courses/" + courseId + "/labs/" + lab.id).then((response) => {
+                api.delete("/api/courses/" + courseId + "/labs/" + lab.id).then((response) => {
                     // http success, call the mutator and change something in state
-          commit(DELETE_LAB, lab)
-          dispatch('loadLabs', courseId)
-                    // Let the calling function know that http is done. You may send some data back
-                    //  push('/apps/manager/courses')
-          resolve(response);
-        }, (error) => {
+                    commit(DELETE_LAB, lab)
+                    dispatch('loadLabs', courseId)
+                        // Let the calling function know that http is done. You may send some data back
+                        //  push('/apps/manager/courses')
+                    resolve(response);
+                }, (error) => {
                     // http failed, let the calling function know that action did not work out
-          commit(API_FAILURE, error);
+                    commit(API_FAILURE, error);
                     //  push('/apps/manager/courses')
-          reject(error);
-        })
-      });
-    },
+                    reject(error);
+                })
+            });
+        },
 
-  },
-  state: {
-    labs: [],
-  },
+    },
+    state: {
+        labs: [],
+    },
 }

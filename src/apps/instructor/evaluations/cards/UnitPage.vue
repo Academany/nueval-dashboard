85db6ecbf85b0a043807ea828f8b7e69ff65f08b
Fiore Basile <fiore.basile@gmail.com>
2018-12-05 14:09:09 +0100

    formatting

--- src/apps/instructor/evaluations/cards/UnitPage.vue
+++ src/apps/instructor/evaluations/cards/UnitPage.vue
@@ -1,255 +1,267 @@
 <template>
-    <el-card>
-        <div slot="header">{{module.name}}
-            <div class="pull-right">
+  <el-card>
+    <div slot="header">
+      {{module.name}}
+      <div class="pull-right">
+        <el-tag v-if="module">Ver. {{module.rules && module.rules.version}}</el-tag>
+      </div>
+    </div>
+    <div class="card-body">
+      <h4>Overall progress</h4>
+      <UnitStatus
+        :readonly="readonly"
+        :module="module"
+        :record="record"
+        @markNotComplete="handleMarkNotComplete"
+        @markComplete="handleMarkComplete"
+        @showFeedback="handleShowFeedback"
+      />
 
-                <el-tag v-if="module">Ver. {{module.rules && module.rules.version}}</el-tag>
-            </div>
-        </div>
-        <div class="card-body">
-            <h4>Overall progress</h4>
-            <UnitStatus :readonly="readonly" :module="module" :record="record" @markNotComplete="handleMarkNotComplete" @markComplete="handleMarkComplete" @showFeedback="handleShowFeedback" />
+      <h4>Unit description</h4>
+      <UnitTasks :module="module"/>
 
-            <h4>Unit description</h4>
-            <UnitTasks :module="module" />
+      <h4>Learning outcomes</h4>
+      <UnitOutcomes
+        :readonly="readonly"
+        :module="module"
+        :record="record"
+        @updateProgress="handleUpdateProgress"
+        @save="handleSaveProgress"
+      />
 
-            <h4>Learning outcomes</h4>
-            <UnitOutcomes :readonly="readonly" :module="module" :record="record" @updateProgress="handleUpdateProgress" @save="handleSaveProgress" />
+      <div v-if="global != true">
+        <h4>Checklist</h4>
+        <StudentCheckList
+          :readonly="readonly"
+          :module="module"
+          :record="record"
+          @save="handleUpdateCheckList"
+        />
+      </div>
 
-            <div v-if="global != true">
-                <h4>Have you?</h4>
-                <StudentCheckList :readonly="readonly" :module="module" :record="record" @save="handleUpdateCheckList" />
-            </div>
+      <h4>Feedback</h4>
+      <FeedbackList ref="FeedbackForm" :messages="getMessages()"/>
 
-            <h4>Feedback</h4>
-            <FeedbackList ref="FeedbackForm" :messages="getMessages()" />
+      <FeedbackForm @newFeedback="handleNewFeedback"/>
 
-            <FeedbackForm @newFeedback="handleNewFeedback" />
+      <h4>FAQ</h4>
+      <UnitFAQ :module="module"/>
 
-            <h4>FAQ</h4>
-            <UnitFAQ :module="module" />
-
-            <h4>Recent Changes</h4>
-            <RecentChanges :record="record" />
-
-        </div>
-
-    </el-card>
+      <h4>Recent Changes</h4>
+      <RecentChanges :record="record"/>
+    </div>
+  </el-card>
 </template>
 
 <script>
-import StudentCheckList from '../fragments/StudentCheckList.vue'
+import StudentCheckList from "../fragments/StudentCheckList.vue";
 
-import FeedbackList from '../forms/FeedbackList.vue'
-import FeedbackForm from '../forms/FeedbackForm.vue'
-import UnitFAQ from '../fragments/UnitFAQ.vue'
-import UnitOutcomes from '../fragments/UnitOutcomes.vue'
-import UnitTasks from '../fragments/UnitTasks.vue'
-import UnitStatus from '../cards/UnitStatus.vue'
-import RecentChanges from '../fragments/RecentChanges.vue'
+import FeedbackList from "../forms/FeedbackList.vue";
+import FeedbackForm from "../forms/FeedbackForm.vue";
+import UnitFAQ from "../fragments/UnitFAQ.vue";
+import UnitOutcomes from "../fragments/UnitOutcomes.vue";
+import UnitTasks from "../fragments/UnitTasks.vue";
+import UnitStatus from "../cards/UnitStatus.vue";
+import RecentChanges from "../fragments/RecentChanges.vue";
 
-import { mapActions, mapGetters } from 'vuex'
-import VueScrollTo from 'vue-scrollto'
+import { mapActions, mapGetters } from "vuex";
+import VueScrollTo from "vue-scrollto";
 
 export default {
-    props: [
-        'module',
-        'record',
-        'readonly',
-        'global'
-    ],
-    components: {
-        UnitFAQ,
-        UnitOutcomes,
-        UnitTasks,
-        UnitStatus,
-        FeedbackForm,
-        FeedbackList,
-        StudentCheckList,
-        RecentChanges
+  props: ["module", "record", "readonly", "global"],
+  components: {
+    UnitFAQ,
+    UnitOutcomes,
+    UnitTasks,
+    UnitStatus,
+    FeedbackForm,
+    FeedbackList,
+    StudentCheckList,
+    RecentChanges
+  },
+  data() {
+    return {
+      selectedTab: "status"
+    };
+  },
+  computed: {
+    ...mapGetters({
+      messages: "instructor_app/records/messages"
+    })
+  },
+  methods: {
+    getMessages() {
+      // console.log('getMessages')
+      if (this.record && this.record.messages) {
+        return this.record.messages;
+      }
+      return [];
     },
-    data() {
-        return {
-            selectedTab: 'status'
-        }
+    handleClick(tab, event) {},
+    handleShowFeedback() {
+      // const tabs = this.$refs.tabs
+      // this.selectedTab = 'feedback'
+      const el = this.$refs.FeedbackForm.$el;
+      el.scrollIntoView(true);
     },
-    computed: {
-        ...mapGetters({
-            messages: 'instructor_app/records/messages'
+    triggerUpdate() {
+      this.$emit("refresh", { module: this.module, record: this.record });
+    },
+    handleUpdateProgress() {},
+    handleSaveProgress(progress) {
+      var vm = this;
+      vm.updateProgress(progress)
+        .then(success => {
+          vm.logOk("Progress recorded successfully");
+          // vm.$nextTick(function() {
+          vm.triggerUpdate();
+
+          // })
         })
+        .catch(error => {
+          vm.logError(error);
+        });
     },
-    methods: {
-        getMessages() {
-            // console.log('getMessages')
-            if (this.record && this.record.messages) {
-                return this.record.messages
-            }
-            return []
-        },
-        handleClick(tab, event) {
+    handleUpdateCheckList(checklist) {
+      var vm = this;
+      vm.updateChecklist(checklist)
+        .then(success => {
+          vm.logOk("Checklist recorded successfully");
+          // vm.$nextTick(function() {
+          vm.triggerUpdate();
 
-        },
-        handleShowFeedback() {
-            // const tabs = this.$refs.tabs
-            // this.selectedTab = 'feedback'
-            const el = this.$refs.FeedbackForm.$el
-            el.scrollIntoView(true)
-        },
-        triggerUpdate() {
-            this.$emit('refresh', { module: this.module, record: this.record })
-        },
-        handleUpdateProgress() {
-
-        },
-        handleSaveProgress(progress) {
-            var vm = this;
-            vm.updateProgress(progress)
-                .then((success) => {
-                    vm.logOk("Progress recorded successfully")
-                    // vm.$nextTick(function() {
-                    vm.triggerUpdate()
-
-                    // })
-                }).catch((error) => {
-                    vm.logError(error)
-                })
-        },
-        handleUpdateCheckList(checklist) {
-            var vm = this;
-            vm.updateChecklist(checklist)
-                .then((success) => {
-                    vm.logOk("Checklist recorded successfully")
-                    // vm.$nextTick(function() {
-                    vm.triggerUpdate()
-
-                    // })
-                }).catch((error) => {
-                    vm.logError(error)
-                })
-        },
-        handleMarkComplete(checklist) {
-            var vm = this;
-            vm.markComplete(checklist)
-                .then((success) => {
-                    vm.logOk("Unit marked complete")
-                    // vm.$nextTick(function() {
-                    vm.triggerUpdate()
-
-                    // })
-                }).catch((error) => {
-                    vm.logError(error)
-                })
-        },
-        handleMarkNotComplete(checklist) {
-            var vm = this;
-            vm.markNotComplete(checklist)
-                .then((success) => {
-                    vm.logOk("Unit is not complete")
-                    // vm.$nextTick(function() {
-                    vm.triggerUpdate()
-                    // })
-                }).catch((error) => {
-                    vm.logError(error)
-                })
-        },
-        handleNewFeedback(feedback) {
-            var vm = this;
-            vm.leaveFeedback(feedback)
-                .then((success) => {
-                    vm.logOk("Feedback recorded successfully")
-                    // vm.$nextTick(function() {
-                    vm.triggerUpdate()
-                    // })
-                }).catch((error) => {
-                    vm.logError(error)
-                })
-        },
-        logError(err) {
-            this.$notify({
-                title: "Error",
-                message: JSON.stringify(err)//message: "There was an error processing your request, please try later"
-            })
-        },
-        logOk(msg) {
-            this.$notify({
-                title: "Information",
-                message: msg
-            })
-        },
-        ...mapActions({
-            markNotComplete: 'instructor_app/records/markNotComplete',
-            updateProgress: 'instructor_app/records/updateProgress',
-            updateChecklist: 'instructor_app/records/updateChecklist',
-            markComplete: 'instructor_app/records/markComplete',
-            leaveFeedback: 'instructor_app/records/leaveFeedback'
+          // })
         })
-    }
-}
+        .catch(error => {
+          vm.logError(error);
+        });
+    },
+    handleMarkComplete(checklist) {
+      var vm = this;
+      vm.markComplete(checklist)
+        .then(success => {
+          vm.logOk("Unit marked complete");
+          // vm.$nextTick(function() {
+          vm.triggerUpdate();
+
+          // })
+        })
+        .catch(error => {
+          vm.logError(error);
+        });
+    },
+    handleMarkNotComplete(checklist) {
+      var vm = this;
+      vm.markNotComplete(checklist)
+        .then(success => {
+          vm.logOk("Unit is not complete");
+          // vm.$nextTick(function() {
+          vm.triggerUpdate();
+          // })
+        })
+        .catch(error => {
+          vm.logError(error);
+        });
+    },
+    handleNewFeedback(feedback) {
+      var vm = this;
+      vm.leaveFeedback(feedback)
+        .then(success => {
+          vm.logOk("Feedback recorded successfully");
+          // vm.$nextTick(function() {
+          vm.triggerUpdate();
+          // })
+        })
+        .catch(error => {
+          vm.logError(error);
+        });
+    },
+    logError(err) {
+      this.$notify({
+        title: "Error",
+        message: JSON.stringify(err) //message: "There was an error processing your request, please try later"
+      });
+    },
+    logOk(msg) {
+      this.$notify({
+        title: "Information",
+        message: msg
+      });
+    },
+    ...mapActions({
+      markNotComplete: "instructor_app/records/markNotComplete",
+      updateProgress: "instructor_app/records/updateProgress",
+      updateChecklist: "instructor_app/records/updateChecklist",
+      markComplete: "instructor_app/records/markComplete",
+      leaveFeedback: "instructor_app/records/leaveFeedback"
+    })
+  }
+};
 </script>
 
 <style scoped>
 .feedback {
-    margin-bottom: 12px;
+  margin-bottom: 12px;
 }
 
 .feedback-header {
-    clear: both;
-    vertical-align: bottom;
-    margin-bottom: 14px;
+  clear: both;
+  vertical-align: bottom;
+  margin-bottom: 14px;
 }
 
 .feedback-title {
-    font-weight: bold;
-    display: block;
-    margin-left: 54px;
-    margin-top: -50px;
+  font-weight: bold;
+  display: block;
+  margin-left: 54px;
+  margin-top: -50px;
 }
 
 .feedback-description {
-    font-size: 0.9em;
-    padding-left: 0px;
+  font-size: 0.9em;
+  padding-left: 0px;
 }
 
 .feedback-icon {
-    display: inline;
+  display: inline;
 }
 
 .micro {
-    width: 48px;
-    height: 48px;
-    border-radius: 24px;
+  width: 48px;
+  height: 48px;
+  border-radius: 24px;
 }
 
 .slider-pad {
-    width: 90%;
-    margin-left: 8px
+  width: 90%;
+  margin-left: 8px;
 }
 
 .label {
-    font-size: .8em;
+  font-size: 0.8em;
 }
 
 .fa-fix {
-    margin-bottom: -2px;
+  margin-bottom: -2px;
 }
 
 .title {
-    margin-top: -32px;
-    margin-left: 32px;
+  margin-top: -32px;
+  margin-left: 32px;
 }
 
 .description {
-    margin-left: 32px;
+  margin-left: 32px;
 }
 
 .pull-right {
-    float: right
+  float: right;
 }
 
 h4 {
-    border-bottom: 1px solid #dedede;
-    font-size: 1.2em;
-    color: #666;
-    text-transform: uppercase;
+  border-bottom: 1px solid #dedede;
+  font-size: 1.2em;
+  color: #666;
+  text-transform: uppercase;
 }
 </style>
